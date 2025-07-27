0be4d9ae46e07e55da33c39534c85f15e0dddcf9
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-07-28 01:08:21 +0800

    feat(ui): testing new sidebar design

---
+++ src/app/components/TestSidebar.tsx
@@ -0,0 +1,14 @@
+export interface TestSidebarProps {
+  icon: React.ReactNode
+  label: string
+  onClick: () => void
+  href?: string
+  active?: 
+}
+function TestSidebar() {
+  return (
+    <div></div>
+  )
+}
+
+export default TestSidebar
