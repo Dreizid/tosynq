16474c70ae4db3cfffa3bdaa2b4cac320caa732f
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-01 20:34:15 +0800

    feat(ui): implemented a simple sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
 "use client"
-import Sidebar from "./components/Sidebar"
 import { usePathname } from "next/navigation"
 
 function page() {
   return (
     <div className="flex flex-row">
-      <Sidebar
-        categories={[
-          {
-            title: "Calendar",
-            links: [{
-              label: "Today",
-              href: "/"
-            }]
-          }
-        ]}
-        currentPath={usePathname()}
-      />
       <div>
       </div>
     </div>
