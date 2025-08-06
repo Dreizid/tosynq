bcb2973b5a16c62717d162a5ca6b59fc9cbf74af
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-06 10:40:39 +0800

    feat(ui): functional big calendar

--- src/app/components/AppCalendar.tsx
+++ src/app/components/AppCalendar.tsx
@@ -1,16 +1,28 @@
-import { Calendar, momentLocalizer } from "react-big-calendar"
+"use client"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from "moment"
 
 const localizer = momentLocalizer(moment)
 
-export function AppCalendar(props: {}) {
+export default function AppCalendar(props: {}) {
+  const events = [
+    {
+      start: new Date(),
+      end: new Date(),
+      title: "Sample Event",
+    },
+  ];
   return (
     <div className="h-full">
       <Calendar
         localizer={localizer}
+        events={events}
         startAccessor="start"
         endAccessor="end"
       />
     </div>
   )
 }
+
+
