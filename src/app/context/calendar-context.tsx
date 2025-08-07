791c6b28956af2c2283ee4ddc333412cd86610dc
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-07 23:58:42 +0800

    feat(calendar): added calendar context to communicate between sidebar and page

---
+++ src/app/context/calendar-context.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { createContext, useState, useContext } from "react";
+import { View } from "react-big-calendar";
+
+type CalendarContextType = {
+  view: View
+  date: Date
+  setView: (view: View) => void
+  setDate: (date: Date) => void
+}
+
+const CalendarContext = createContext<CalendarContextType | null>(null)
+
+export const useCalendar = () => {
+  const ctx = useContext(CalendarContext)
+  if (!ctx) throw new Error('useCalendar must be used within CalendarProvider')
+  return ctx
+}
+
+export const CalendarProvider = ({ children }: { children: React.ReactNode }) => {
+  const [view, setView] = useState<View>('month')
+  const [date, setDate] = useState<Date>(new Date())
+
+  return (
+    <CalendarContext.Provider value={{ view, date, setView, setDate }}>
+      {children}
+    </CalendarContext.Provider>
+  )
+
+}
