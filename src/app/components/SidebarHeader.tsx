40096e7a8d64b2831175f31deb83def55e380f12
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-12 18:58:32 +0800

    fix: header's z index increased so it doesn't go below the page content

--- src/app/components/SidebarHeader.tsx
+++ src/app/components/SidebarHeader.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbPage } from "@/components/ui/breadcrumb";
 import { useCalendar } from "../context/calendar-context";
 
-export default function SidebarHeader() {
+export default function SidebarHeader({ }) {
   const { date } = useCalendar()
   const options = {
     weekday: "long",
@@ -13,7 +13,7 @@ export default function SidebarHeader() {
     day: "numeric",
   };
   return (
-    <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+    <header className="bg-background sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
       <Separator
         orientation="vertical"
