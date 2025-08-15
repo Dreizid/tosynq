7e993f57382b8160c44f33d22fa912666df58152
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-16 03:12:02 +0800

    fix: update imports after previous refactor

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
 'use client'
-import AddTaskButton from "../components/AddTaskButton"
+import FloatingActionButton from "@/app/components/FloatingActionButton"
+import TaskFormDialog from "@/app/components/TaskFormDialog"
 import AppCalendar from "../components/AppCalendar"
 import { useCalendar } from "../context/calendar-context"
+import { useState } from "react"
 function page() {
   const { date, view } = useCalendar()
+  const [open, setOpen] = useState(false)
   return (
     <div className="h-full">
       <AppCalendar date={date} view={view} />
-      <AddTaskButton />
+      <FloatingActionButton onClick={() => setOpen(true)} />
+      <TaskFormDialog open={open} onOpenChange={() => setOpen(false)} />
     </div>
   )
 }
