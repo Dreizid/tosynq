3f3fbbbf9e8721b74e2b3e4e3840d2dab77a5304
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-16 03:10:38 +0800

    refactor: split AddTaskButton into FloatingActionButton and TaskFormDialog

---
+++ src/app/components/FloatingActionButton.tsx
@@ -0,0 +1,20 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { Plus } from "lucide-react"
+function FloatingActionButton({ onClick }: { onClick: () => void }) {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild >
+        <button
+          onClick={onClick}
+          className="fixed bottom-6 right-6 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-background shadow-lg hover:scale-120 transition-transform"
+        >
+          <Plus />
+        </button>
+      </TooltipTrigger>
+      <TooltipContent className="bg-background fill-background text-primary shadow-lg">
+        <p>Add a task</p>
+      </TooltipContent>
+    </Tooltip>
+  )
+}
+export default FloatingActionButton;
