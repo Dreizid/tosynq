c17d91b1f9a17f79c3137680d554b7255d4cb16e
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-17 02:38:38 +0800

    fix: updated props to match refactored floating action button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,13 +4,15 @@ import TaskFormDialog from "@/app/components/TaskFormDialog"
 import AppCalendar from "../components/AppCalendar"
 import { useCalendar } from "../context/calendar-context"
 import { useState } from "react"
+import { Plus } from "lucide-react"
+import { DateTimePicker } from "../components/DateTimePicker"
 function page() {
   const { date, view } = useCalendar()
   const [open, setOpen] = useState(false)
   return (
     <div className="h-full">
       <AppCalendar date={date} view={view} />
-      <FloatingActionButton onClick={() => setOpen(true)} />
+      <FloatingActionButton onClick={() => setOpen(true)} icon={Plus} />
       <TaskFormDialog open={open} onOpenChange={() => setOpen(false)} />
     </div>
   )
