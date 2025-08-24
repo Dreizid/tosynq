976e8bfaa4b8dedbad4b44946da7a3051bfb9cec
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-08-24 23:22:53 +0800

    style: change header font weight and style

--- src/app/components/SidebarHeader.tsx
+++ src/app/components/SidebarHeader.tsx
@@ -9,28 +9,20 @@ import ListSelector from "./ListSelector";
 export default function SidebarHeader({ }) {
   const { date } = useCalendar()
   const options = {
-    weekday: "long",
     year: "numeric",
     month: "long",
-    day: "numeric",
   };
   return (
-    <header className="bg-background sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b px-4">
-      <SidebarTrigger className="-ml-1" />
-      <Separator
-        orientation="vertical"
-        className="mr-2 data-[orientation=vertical]:h-4"
-      />
+    <header className="bg-background sticky top-0 z-10 flex h-16 shrink-0 mt-2 items-center gap-2 px-4">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
-            <BreadcrumbPage>{date.toLocaleDateString("en-US", options)}</BreadcrumbPage>
+            <BreadcrumbPage className="text-2xl mx-2 font-bold">{date.toLocaleDateString("en-US", options)}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
       <div className="flex h-full items-center ml-auto">
         <ListSelector />
-        <Separator orientation="vertical" className="mx-2" />
         <ViewSelector />
       </div>
     </header>
