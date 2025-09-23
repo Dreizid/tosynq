c93e370fde7b92a20cb3d503db40898da66e89fc
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-09-23 17:00:48 +0800

    refactor(forms): extract button component to allow for an optional tooltip

--- src/app/components/form/FloatingActionButton.tsx
+++ src/app/components/form/FloatingActionButton.tsx
@@ -1,20 +1,31 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 import { LucideIcon } from "lucide-react"
 function FloatingActionButton({ onClick, icon: Icon }: { onClick: () => void, icon: LucideIcon }) {
+interface FloatingActionButtonProps {
+  onClick: () => void
+  icon: LucideIcon
+  tooltip?: string
+}
+function FloatingActionButton({ onClick, icon: Icon, tooltip }: FloatingActionButtonProps) {
+  const button = (
+    <button
+      onClick={onClick}
+      className="fixed bottom-6 right-6 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-background shadow-lg hover:scale-120 transition-transform"
+    >
+      <Icon />
+    </button>
+  )
   return (
-    <Tooltip>
-      <TooltipTrigger asChild >
-        <button
-          onClick={onClick}
-          className="fixed bottom-6 right-6 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-background shadow-lg hover:scale-120 transition-transform"
-        >
-          <Icon />
-        </button>
-      </TooltipTrigger>
-      <TooltipContent className="bg-background fill-background text-primary shadow-lg">
-        <p>Add a task</p>
-      </TooltipContent>
-    </Tooltip>
+    tooltip ? (
+      <Tooltip>
+        <TooltipTrigger asChild >
+          {button}
+        </TooltipTrigger>
+        <TooltipContent className="bg-background fill-background text-primary shadow-lg">
+          <p>{tooltip}</p>
+        </TooltipContent>
+      </Tooltip >
+    ) : (button)
   )
 }
 export default FloatingActionButton;
