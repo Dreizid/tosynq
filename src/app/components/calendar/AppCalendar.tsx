2e27da5a7529a7d67e2648936e114f99790acb4b
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-10-05 16:49:04 +0800

    refactor(calendar): seperate useResizeObserver into its own file

--- src/app/components/calendar/AppCalendar.tsx
+++ src/app/components/calendar/AppCalendar.tsx
@@ -2,7 +2,7 @@
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "../../lib/db/dexie";
-import { useRef, useEffect, useMemo, useState, useLayoutEffect } from "react";
+import { useRef, useMemo, useLayoutEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import { CalendarApi } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -10,6 +10,7 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import EventComponent from "@/app/components/calendar/EventComponent";
 import { useCalendar } from "../../context/calendar-context";
 import { TaskType, SourceType } from "@/app/lib/db/dexie";
+import { useResizeObserver } from "@/app/hooks/useResizeObserver";
 
 export type View =
   | "dayGridDay"
@@ -30,27 +31,6 @@ export type EventExtendedProps = {
   description: string;
   deleted: boolean;
 };
-
-// Subject to testing
-/**
- * A custom hook that fire's the `callback` when the `ref` changes.
- *
- * @param ref - A React ref object to watch.
- * @param callback - A function to call whenever the `ref.current` value changes.
- */
-function useResizeObserver<T extends HTMLElement>(
-  ref: React.RefObject<T | null>,
-  callback: () => void,
-) {
-  useEffect(() => {
-    if (!ref.current) return;
-    const resizeObserver = new ResizeObserver(callback);
-    resizeObserver.observe(ref.current);
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [ref, callback]);
-}
 
 export default function AppCalendar({ date, range }: AppCalendarProps) {
   const { enabled } = useCalendar();
