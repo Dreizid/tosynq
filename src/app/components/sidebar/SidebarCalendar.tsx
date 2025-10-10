cf8fe1ea6250c3e1820eacd6636bf3952c0d93f1
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-10-10 09:50:32 +0800

    fix: sync main calendar when month changes in main calendar
    fix: update docs and remove nullish operator since date object is always
    initialized

--- src/app/components/sidebar/SidebarCalendar.tsx
+++ src/app/components/sidebar/SidebarCalendar.tsx
@@ -3,15 +3,24 @@ import { View } from "@/app/components/calendar/AppCalendar";
 import { Calendar } from "@/components/ui/calendar";
 import { SidebarGroup } from "@/components/ui/sidebar";
 import { endOfWeek, startOfWeek } from "date-fns";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { DateRange, DayPickerProps } from "react-day-picker";
 import { useCalendar } from "@/app/context/calendar-context";
 
+/**
+ * Returns a Date object representing the last day of the given month.
+ * @param month - A Date object for the last day of the mmonth
+ */
+function getLastDayOfMonth(month: Date): Date {
+  const lastDay = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+  return lastDay;
+}
 export default function SidebarCalendar() {
   /* TO DO:
   Add accent colors for current day in calendar 
   */
   const { date, range, setDate, setRange } = useCalendar();
+  const [intendedDate, setIntendedDate] = useState<number>(date.getDate());
 
   const modifiers = useMemo(() => {
     switch (range) {
@@ -62,7 +71,18 @@ export default function SidebarCalendar() {
         className="[&_[role=gridcell].bg-accent]:bg-background [&_[role=gridcell].bg-accent]:text-secondary-foreground [&_[role=gridcell].bg-accent]:w-full block p-0 bg-sidebar-background [&_[role=gridcell]>button]:outline-none [&_[role=gridcell]>button]:ring-0"
         classNames={classNameMap[range]}
         modifiers={modifiers}
-        onDayClick={(day) => onViewClick(day)}
+        onDayClick={(day) => {
+          setIntendedDate(day.getDate());
+          onViewClick(day);
+        }}
+        onMonthChange={(month) => {
+          const newDate = new Date(
+            month.getFullYear(),
+            month.getMonth(),
+            Math.min(getLastDayOfMonth(month).getDate(), intendedDate),
+          );
+          onViewClick(newDate);
+        }}
       />
     </SidebarGroup>
   );
